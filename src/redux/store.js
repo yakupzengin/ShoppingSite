00b2760e56ad746b55a5447dd5ddd2c99e2672e1
Yakup Zengin <118113891+yakupzengin@users.noreply.github.com>
2023-06-20 13:42:05 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,9 @@ import {configureStore} from "@reduxjs/toolkit";
 import CategorySlice from "./CategorySlice";
 import productSlice from "./productSlice";
 import cardSlice from "./cardSlice";
+/*
 import SearchSlice from "./SearchSlice";
+*/
 export const store = configureStore({
     reducer: {
         categories : CategorySlice,
